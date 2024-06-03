2ade37f913312fb9f1ee17f041bdac27979dae79
Souvik Mondal <66114859+SouvikM78@users.noreply.github.com>
2024-06-03 18:59:54 +0530

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,10 @@
+Use CreditCard
+Select * from credit_card
+Select * from cc_add
+Insert into credit_card select * from cc_add; -- To add new rows with same column name
+
+Select * from customer
+Select * from cust_add
+Insert into customer select * from cust_add;  -- To add new rows with same column name
+
+
